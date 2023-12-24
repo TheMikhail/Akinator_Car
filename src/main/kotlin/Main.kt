f7ea92f9d64942da9b0c3b6a86e000cb48bacebc
TheMikhail <mdd2002@mail.ru>
2023-12-25 01:46:35 +0700

    Akinator DataClass

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -12,6 +14,7 @@ import androidx.compose.ui.window.application
 import java.util.*
 import java.util.List
 import java.util.function.Predicate
+import kotlin.random.Random
 
 
 @Composable
@@ -20,36 +23,61 @@ fun App() {
     //var text by remember { mutableStateOf("Hello, World!") }
 
     val questionJDM = QuestionJDM()
+    val questionGearboxManual = QuestionGearboxManual()
+    val questionEDM = QuestionEDM()
+    val questionGearboxAutomatic = QuestionGearboxAutomatic()
+    val questionRDM = QuestionRDM()
+    val questionUSDM = QuestionUSDM()
+
+
     val car1 = Car(Manufactory.HONDA,"Civic","manual", listOf(Market.JDM))
     val car2 = Car(Manufactory.VAZ, "2111", "manual", listOf(Market.RDM))
     val car3 = Car(Manufactory.TOYOTA, "Corona", "automatic", listOf(Market.JDM))
     val car4 = Car(Manufactory.MERCEDES, "w140", "manual", listOf(Market.EDM))
     val car5 = Car(Manufactory.BMW, "t39", "manual", listOf(Market.EDM))
-    val carfilter = List.of(car1, car2, car3, car4, car5)
+    val carFilter = List.of(car1, car2, car3, car4, car5)
 
-    var TextQuestion:String
+    //val questionRandom = arrayOf(questionUSDM.description(),questionRDM.description(),questionRDM.description(),
+    //    questionJDM.description(),questionGearboxAutomatic.description(),questionGearboxAutomatic.description())
+
+   // val questionArray = questionRandom.size
+   // val rand = Random.nextInt(questionArray)
     MaterialTheme {
+        //var label = remember { mutableStateOf("${questionRandom[rand]}") }
+        val label = remember { mutableStateOf(questionJDM.description()) }
+
         Column (modifier = Modifier.fillMaxSize().padding(bottom = 35.dp) ,verticalArrangement = Arrangement.Bottom){
             Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
                 TextButton(onClick = {
 
-                    val jdmCar = carfilter.stream().filter(questionJDM.filter() as Predicate<in Car>?).toList()
+                        val jdmCar = carFilter.stream().filter(questionJDM.filter() as Predicate<in Car>?).toList()
+
+                        label.value = questionGearboxManual.description()
+
+
+
 
                 }) {
                     Text(text = "Да", fontSize = 32.sp)
+
+
                 }
 
                 TextButton(onClick = {
 
                 }) {
                     Text(text = "Нет",fontSize = 32.sp)
+
                 }
+
 
             }
         }
         Column (modifier = Modifier.fillMaxSize(),verticalArrangement = Arrangement.Center){
             Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-                Text(text = questionJDM.description(), fontSize = 30.sp)
+                Text(label.value, fontSize = 30.sp)
+
+
             }
         }
 
