bf60557cfc450745be6a3855018ea16e864fc9dd
TheMikhail <mdd2002@mail.ru>
2024-01-17 19:16:58 +0700

    Akinator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -31,7 +31,6 @@ fun app() {
     val currentQuestion = remember { mutableStateOf(SelectNewQuestion.getNextQuestion()) }
     val question = currentQuestion.value
     if (question != null) {
-
         questionItem(question, onAnswer = { answer ->
             filteredCars.value = filteredCars.value.filterByAnswer(question, answer)
             currentQuestion.value = SelectNewQuestion.getNextQuestion()
@@ -47,7 +46,6 @@ fun app() {
         }
     }
 }
-
 object SelectNewQuestion {
     private val remainderQuestion: MutableList<Question> = mutableListOf(
         QuestionJDM, QuestionEDM, QuestionUSDM,
